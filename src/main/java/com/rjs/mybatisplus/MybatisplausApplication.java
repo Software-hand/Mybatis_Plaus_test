ba648387e906b09566a8c429502de7bf9320b489
软件手 <471583744@qq.com>
2022-07-30 01:36:53 +0800

    Initial commit

---
+++ src/main/java/com/rjs/mybatisplus/MybatisplausApplication.java
@@ -0,0 +1,13 @@
+package com.rjs.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisplausApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisplausApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rjs/mybatisplus/MybatisplausApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjs.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisplausApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
